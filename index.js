dca2a7490b74f7380f5c566781110e611aa97222
Or Ashtamker <ashi@MacBook-Pro.local>
2021-04-18 15:45:35 +0300

    add cleint side

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const app = express();
+const cors = require('cors');
 const port = 8000;
 const usersRoute = require("./routes/bankUsers.route");
 
+app.use(cors());
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 app.use("/api/bank", usersRoute);
